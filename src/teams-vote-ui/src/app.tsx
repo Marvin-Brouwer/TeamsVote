77510375dd039239de8cba529b1b520fc332d5b1
Marvin <marvin-brouwer@live.nl>
2026-01-20 14:23:13 +0100

    Moved theme into separate component

--- src/teams-vote-ui/src/app.tsx
+++ src/teams-vote-ui/src/app.tsx
@@ -1,44 +1,11 @@
 import { RouteSectionProps } from '@solidjs/router'
-import { children, createEffect, createSignal, Show, type Component } from 'solid-js'
-import { DesignSystemProvider, fluentAnchoredRegion, fluentBadge, fluentButton, fluentCard, fluentDataGrid, fluentDataGridCell, fluentDataGridRow, fluentOption, fluentProgressRing, fluentSelect, fluentTextField, provideFluentDesignSystem } from '@fluentui/web-components'
+import { children, type Component } from 'solid-js'
+import { Theme } from './theme'
 
 import './app.css'
 
-// TODO move to themecontext
-import { teamsLightTheme } from '@fluentui/tokens';
-
 export const AppRoot: Component<RouteSectionProps> = (props) => {
-
-    const [themeRef, setThemeRef] = createSignal<DesignSystemProvider>();
-    const [themeLoaded, setThemeLoaded] = createSignal(false);
-
-    createEffect(() => {
-        const themeRoot = themeRef();
-        if (!themeRoot) return;
-
-        provideFluentDesignSystem(themeRoot)
-            .register(fluentButton())
-            .register(fluentTextField())
-            .register(fluentSelect(), fluentOption())
-            .register(fluentBadge())
-            .register(fluentCard())
-            .register(
-                fluentDataGridCell(),
-                fluentDataGridRow(),
-                fluentDataGrid()
-            )
-            .register(fluentAnchoredRegion())
-            .register(fluentProgressRing())
-            .withShadowRootMode('open')
-            .withDesignTokenRoot(document);
-
-        setThemeLoaded(true)
-    })
-
-    return <fluent-design-system-provider
-        ref={setThemeRef}
-        design-system={teamsLightTheme}
-    >
-        <Show when={themeLoaded()}>{children(() => props.children)()}</Show>
-    </fluent-design-system-provider>
+    return <Theme>
+        {children(() => props.children)()}
+    </Theme>
 }
---
+++ src/teams-vote-ui/src/theme.tsx
@@ -0,0 +1,47 @@
+import { children, createEffect, createSignal, Show, type ParentComponent } from 'solid-js'
+import * as component from '@fluentui/web-components'
+import { DesignSystemProvider, provideFluentDesignSystem } from '@fluentui/web-components'
+import { teamsLightTheme } from '@fluentui/tokens';
+import { type DesignSystem } from '@microsoft/fast-foundation';
+
+import "./theme.css";
+
+function registerComponents(system: DesignSystem): DesignSystem {
+    return system
+        .register(component.fluentButton())
+        .register(component.fluentTextField())
+        .register(component.fluentSelect(), component.fluentOption())
+        .register(component.fluentBadge())
+        .register(component.fluentCard())
+        .register(
+            component.fluentDataGridCell(),
+            component.fluentDataGridRow(),
+            component.fluentDataGrid()
+        )
+        .register(component.fluentAnchoredRegion())
+        .register(component.fluentProgressRing())
+}
+
+export const Theme: ParentComponent = (props) => {
+
+    const [themeRef, setThemeRef] = createSignal<DesignSystemProvider>();
+    const [themeLoaded, setThemeLoaded] = createSignal(false);
+
+    createEffect(() => {
+        const themeRoot = themeRef();
+        if (!themeRoot) return;
+
+        registerComponents(provideFluentDesignSystem(themeRoot))
+            .withShadowRootMode('open')
+            .withDesignTokenRoot(document);
+
+        setThemeLoaded(true)
+    })
+
+    return <fluent-design-system-provider
+        ref={setThemeRef}
+        design-system={teamsLightTheme}
+    >
+        <Show when={themeLoaded()}>{children(() => props.children)()}</Show>
+    </fluent-design-system-provider>
+}
