bfdc8685b0c06c2643993ec687b80a9ac9e163f5
Marvin <marvin-brouwer@live.nl>
2026-01-26 16:39:04 +0100

    maybe like this?

--- src/teams-vote-ui/src/pages/tab.tsx
+++ src/teams-vote-ui/src/pages/tab.tsx
@@ -94,8 +94,7 @@ export const TabView: Component = () => {
                 tenantId: import.meta.env.VITE_TENANT,
                 resources: [
                     location.href,
-                    "api://06899032-7fae-4792-b084-ab75b2caa73f/tab",
-                    "api://marvin-brouwer.github.io/06899032-7fae-4792-b084-ab75b2caa73f",
+                    "api://06899032-7fae-4792-b084-ab75b2caa73f/tab"
                     // TODO maybe request Graph token
                 ]
             });
