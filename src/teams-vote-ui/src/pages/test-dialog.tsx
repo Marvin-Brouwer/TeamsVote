ffd00092720681b82c0a17248c379558f92894be
Marvin <marvin-brouwer@live.nl>
2026-01-27 18:13:52 +0100

    testing result.success

--- src/teams-vote-ui/src/pages/test-dialog.tsx
+++ src/teams-vote-ui/src/pages/test-dialog.tsx
@@ -3,6 +3,7 @@ import type { Component } from "solid-js";
 
 export const TestDialog: Component = () => <div class="view loading-spinner">
     <fluent-button onClick={() => dialog.url.submit({
+        result: "success",
         action: "startVote",
         estimate: 5
     })}>
